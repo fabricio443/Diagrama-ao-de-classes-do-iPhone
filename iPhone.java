980c803c0645d63b179356df7d87c4248b802bef
fabricio <fabriciohcosta@outlook.com.br>
2025-07-09 15:17:07 -0300

    Diagramaçao de classes do iPhone

---
+++ iPhone.java
@@ -0,0 +1,66 @@
+public class iPhone implements ReprodutorMusical, AparelhoTelefonico, NavegadorInternet {
+
+    @Override
+    public void tocar() {
+        System.out.println("Tocando música...");
+    }
+
+    @Override
+    public void pausar() {
+        System.out.println("Música pausada.");
+    }
+
+    @Override
+    public void avancar() {
+        System.out.println("Avançando para a próxima música...");
+    }
+
+    @Override
+    public void retroceder() {
+        System.out.println("Voltando para a música anterior...");
+    }
+
+    @Override
+    public void ligar(String numero) {
+        System.out.println("Ligando para " + numero + "...");
+    }
+
+    @Override
+    public void atender() {
+        System.out.println("Atendendo chamada...");
+    }
+
+    @Override
+    public void iniciarCorreioVoz() {
+        System.out.println("Iniciando correio de voz...");
+    }
+
+    @Override
+    public void exibirPagina(String url) {
+        System.out.println("Exibindo página: " + url);
+    }
+
+    @Override
+    public void adicionarNovaAba() {
+        System.out.println("Nova aba adicionada.");
+    }
+
+    @Override
+    public void atualizarPagina() {
+        System.out.println("Atualizando página...");
+    }
+
+    public static void main(String[] args) {
+        iPhone meuIphone = new iPhone();
+
+        meuIphone.tocar();
+        meuIphone.pausar();
+
+        meuIphone.ligar("1234-5678");
+        meuIphone.atender();
+
+        meuIphone.exibirPagina("https://www.apple.com");
+        meuIphone.adicionarNovaAba();
+        meuIphone.atualizarPagina();
+    }
+}
